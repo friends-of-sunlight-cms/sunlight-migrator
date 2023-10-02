42e736b3da57d4bc0642a0db94c5e499afa9560e
jDanek <jirka.danek@jdanek.eu>
2023-10-02 22:32:15 +0200

    Fix error "Can't specify target table for update in FROM clause"

--- migration/patches/20230527210617_update_users_data.sql
+++ migration/patches/20230527210617_update_users_data.sql
@@ -5,15 +5,11 @@ UPDATE `sunlight_user` SET `publicname`=NULL WHERE publicname='';
 UPDATE `sunlight_user` SET `password`=CONCAT('md5_legacy:0:', `salt`, ':', `password`);
 ALTER TABLE `sunlight_user` DROP COLUMN `salt`;
 
-ALTER TABLE `sunlight_user` ADD COLUMN `migr_move` TINYINT(1) NOT NULL DEFAULT 0;
-UPDATE `sunlight_user` SET `migr_move`=1 WHERE id=0;
+SET @new_admin_id := (SELECT MAX(id)+1 FROM `sunlight_user`);
+UPDATE `sunlight_user` SET `id`=@new_admin_id WHERE id=0;
 
-UPDATE `sunlight_user` SET `id`=(SELECT MAX(id)+1 FROM `sunlight_user`) WHERE id=0;
-
-UPDATE `sunlight_article` SET `author`=(SELECT id FROM `sunlight_user` WHERE migr_move=1) WHERE author=0;
-UPDATE `sunlight_pm` SET `sender`=(SELECT id FROM `sunlight_user` WHERE migr_move=1) WHERE sender=0;
-UPDATE `sunlight_pm` SET `receiver`=(SELECT id FROM `sunlight_user` WHERE migr_move=1) WHERE receiver=0;
-UPDATE `sunlight_poll` SET `author`=(SELECT id FROM `sunlight_user` WHERE migr_move=1) WHERE author=0;
-UPDATE `sunlight_post` SET `author`=(SELECT id FROM `sunlight_user` WHERE migr_move=1) WHERE author=0;
-
-ALTER TABLE `sunlight_user` DROP COLUMN `migr_move`;
+UPDATE `sunlight_article` SET `author`=@new_admin_id WHERE author=0;
+UPDATE `sunlight_pm` SET `sender`=@new_admin_id WHERE sender=0;
+UPDATE `sunlight_pm` SET `receiver`=@new_admin_id WHERE receiver=0;
+UPDATE `sunlight_poll` SET `author`=@new_admin_id WHERE author=0;
+UPDATE `sunlight_post` SET `author`=@new_admin_id WHERE author=0;
